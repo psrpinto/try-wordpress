3edffffa6a57b4b948bb93a6d6f76f9476970357
Paulo Pinto <paulo.pinto@automattic.com>
2024-08-07 12:15:18 +0100

    Add a build script

---
+++ scripts/build.js
@@ -0,0 +1,13 @@
+let shell = require("shelljs");
+let run = (cmd) => {
+    shell.exec(cmd, {
+        env: {...process.env, FORCE_COLOR: true},
+    });
+};
+
+let browser = process.argv.slice(2)[0];
+
+run(`wp-scripts build --webpack-src-dir=src/background --output-path=${browser}/background`);
+run(`wp-scripts build --webpack-src-dir=src/content --output-path=${browser}/content`);
+run(`cp -r src/sidebar ${browser}/`);
+run(`wp-scripts build --webpack-src-dir=src/wp-plugin --output-path=${browser}/wp-plugin`);
