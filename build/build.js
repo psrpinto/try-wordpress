ea4f2a1c7e29a11d22c140e180c4bf5323cb4568
Paulo Pinto <paulo.pinto@automattic.com>
2024-08-07 12:28:54 +0100

    Output to a dist/ directory

--- build/build.js
+++ build/build.js
@@ -6,11 +6,13 @@ let run = (cmd) => {
 };
 
 let browser = process.argv.slice(2)[0];
+let target = `dist/${browser}`;
+run(`rm -rf ${target} && mkdir -p ${target}`);
 
-run(`cp build/manifest-${browser}.json ${browser}/manifest.json`);
-run(`cp -r build/icons ${browser}/`);
+run(`cp build/manifest-${browser}.json ${target}/manifest.json`);
+run(`cp -r build/icons ${target}/`);
 
-run(`wp-scripts build --webpack-src-dir=src/background --output-path=${browser}/background`);
-run(`wp-scripts build --webpack-src-dir=src/content --output-path=${browser}/content`);
-run(`cp -r src/sidebar ${browser}/`);
-run(`wp-scripts build --webpack-src-dir=src/wp-plugin --output-path=${browser}/wp-plugin`);
+run(`wp-scripts build --webpack-src-dir=src/background --output-path=${target}/background`);
+run(`wp-scripts build --webpack-src-dir=src/content --output-path=${target}/content`);
+run(`cp -r src/sidebar ${target}/`);
+run(`wp-scripts build --webpack-src-dir=src/wp-plugin --output-path=${target}/wp-plugin`);
